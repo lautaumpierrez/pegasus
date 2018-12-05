310d05b232e358b9aaa6717633e4f463fd65aeba
Lautaro Umpierrez <lauta.guillermo@gmail.com>
2018-12-05 00:49:53 -0300

    deploy

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 window.onload=()=>{
 	const elements = [
 		{
-			$element: document.getElementById('hello'),
-			hash: '#hello',
-			callback:()=>{
-				alert("adios");
-			}
+			$element: document.getElementById('home'),
+			hash: 'Home',
 		},{
-			$element: document.getElementById('holas'),
-			hash: '#holas',
+			$element: document.getElementById('contacto'),
+			hash: 'Contacto',
+		},{
+			$element: document.getElementById('example-modal'),
+			hash:'example-modal',
 		}
 	]
 	const config = {
 		router: true,
+		multihash: true,
 		template404: document.getElementById('404Template')
 	};
-	const pegasus = new Pegasus({config,defaultHash:'#holas'});
+	const $container = document.getElementById('container');
+	const pegasus = new Pegasus({config,$container,defaultHash:'#Home'});
+	window.pegasus = pegasus;
 	pegasus.saddle(elements);
+	// Events	
+	document.querySelector('.close-pegasus-modal-bg').addEventListener('click',()=>hideModal());
+	document.querySelector('.close-pegasus-modal-cross').addEventListener('click',()=>hideModal());
+	document.getElementById('navbar-burger').addEventListener('click', ()=>{
+		document.getElementById('navbar-menu').classList.toggle('is-active');
+		document.getElementById('navbar-burger').classList.toggle('is-active');
+	});
+	const hideModal = ()=>{
+		let $pegasus_modal = document.querySelector('.pegasus-modal');
+		$pegasus_modal.classList.remove('animated', 'fadeInDown');
+		$pegasus_modal.classList.add('animated', 'fadeOutUp', 'faster');
+		setTimeout(()=>{
+			pegasus.fly('');
+			$pegasus_modal.classList.remove('animated', 'fadeOutUp', 'faster');
+			$pegasus_modal.classList.add('animated', 'fadeInDown');
+		},500);
+	}
 }
+
--- src/pegasus.js
+++ src/pegasus.js
@@ -1,18 +1,25 @@
 class Pegasus {
-	constructor({defaultHash, config}){
+	constructor({defaultHash, config,$container}){
 		this.defaultHash = defaultHash;
+		if(config.multihash == undefined) throw new Error("set the prop <multihash> in the object of configuration");
+		if(config.router == undefined) throw new Error("set the prop <router> in the object of conifguration");
+		if(config.router && config.template404 == undefined) throw new Error("set the 404 template, is needed when use the router mode ...");
+		if(config.template404 != null && !this.isHTMLElement(config.template404))
+			throw new Error('config object need a template404 HTMLElement.');
 		this.config = {
 			router: config.router,
-		}
+			multihash: config.multihash
+		};
+		if(this.isHTMLElement($container)) $container.style['display'] = 'block'; 
 		if(this.config.router)
 			this.$element_error404 = config.template404;
+		this.dataBinding();
 	}
 	verifyDefaultHash(){
-		var thisObj = this;
 		return new Promise((resolve, reject)=>{
 			if(window.location.hash == '')
 			{
-				thisObj.fly(this.defaultHash);
+				this.fly(this.defaultHash);
 				resolve(false);
 			}else{
 				resolve(true);
@@ -20,26 +27,54 @@ class Pegasus {
 		})
 	}
 	saddle(elements=[]){
+		elements.map(element=>{
+			element.hash = element.hash.split('#').join('');
+		});
 		this.elements = elements;
-		this.verifyDefaultHash().then((hasHash)=>{
+		if(this.config.router) this.verifyDefaultHash().then((hasHash)=>{
 			this.changeUrl();
-		})		
+		})
+		else this.changeUrl();
 		window.onpopstate = ()=>this.changeUrl();
 	}
 	changeUrl(){
-		var currentHash = undefined
+		var currentHash = undefined;
+		var multihash =false;
 		if(window.location.hash == null)
 			currentHash = undefined;
-		else
+		else{
+
 			currentHash = window.location.hash;
-		this.dieOrlive(currentHash);
+			currentHash = currentHash.split('#').join('');
+			if(this.config.multihash){
+				if(currentHash.split('&').length > 1)
+				{
+					multihash = true;
+				}
+			}
+		}
+		if(multihash){
+			var hashs = currentHash.split('#').join('');
+			hashs = hashs.split('&');
+			hashs.map((hash)=>{
+				this.dieOrlive({ currentHash: `${hash}`, multihash: true, hashs});
+			})
+		}else{
+			this.dieOrlive({currentHash, multihash,hashs: null});
+		}
 	}
-	dieOrlive(currentHash){
-		const page = this.elements.find(element => element.hash === window.location.hash);
+	dieOrlive({currentHash,multihash,hashs}){
+		var page = undefined;
+		if(!multihash){
+			page = this.elements.find(element => element.hash === currentHash);
+		}else{
+			page = this.elements.find(element => element.hash === currentHash);
+		}
 		if(page == undefined && this.config.router)
 		{
 			this.elements.map(element=>element.$element.style['display'] = 'none');
 			this.error404();
+			throw "ERROR 404"
 		}else{
 			if(this.config.router)
 				this.$element_error404.style['display'] = 'none';
@@ -48,14 +83,21 @@ class Pegasus {
 					if(element.hash == currentHash)
 					{
 						element.$element.style['display'] = 'block';
-
 						if(element.callback == null)
 						{
 							element.callback = ()=>{};
 						}
 						element.callback();
 					}else{
-						element.$element.style['display'] = 'none';
+						if(hashs !== null){
+							var hashelement = hashs.find(hash => element.hash === `${hash}`);
+							if(!hashelement)
+							{
+								element.$element.style['display'] = 'none';
+							}
+						}else{
+							element.$element.style['display'] = 'none';
+						}
 					}
 				}else{
 					element.$element.style['display'] = 'none';
@@ -63,14 +105,53 @@ class Pegasus {
 			});
 		}
 	}
+
 	error404(){
 		this.$element_error404.style['display'] = 'block';
 	}
 	fly(hash){
-		hash = hash.split('#').join('');
-		return window.location.hash = hash;
+		if(hash === undefined) throw new Error('This method need a param Array or String'); 
+		if(hash == '') this.fly(this.defaultHash);
+		else{
+			if(Array.isArray(hash))
+			{
+				var destinyHash = '';
+				hash.map((hash, index)=>{
+					hash = hash.split('#').join('');
+					if(index == 0) destinyHash +=`#${hash}`;
+					else destinyHash +=`&${hash}`;
+				});
+				window.location.hash = destinyHash;
+			}else{
+				hash = hash.split('#').join('');
+				window.location.hash = `#${hash}`;  
+			}
+		}
 	}
 	addElement({$element,hash}){
 		this.elements.push({$element,hash});
+	}
+	isHTMLElement(obj){
+		try {
+			//Using W3 DOM2 (works for FF, Opera and Chrome)
+			return obj instanceof HTMLElement;
+		}
+		catch(e){
+			//Browsers not supporting W3 DOM2 don't have HTMLElement and
+			//an exception is thrown and we end up here. Testing some
+			//properties that all elements have (works on IE7)
+			return (typeof obj==="object") &&
+			(obj.nodeType===1) && (typeof obj.style === "object") &&
+			(typeof obj.ownerDocument ==="object");
+		}
+	}
+	dataBinding(){
+		let $pegasus_fly = document.querySelectorAll('[pegasusfly]');
+		$pegasus_fly.forEach($linkElement=>{
+			let link = $linkElement.getAttribute('pegasusFly');
+			$linkElement.addEventListener('click', ()=>{
+				this.fly(link);
+			})
+		});
 	}
 }
